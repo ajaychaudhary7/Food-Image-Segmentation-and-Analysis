bb43135cb3539629f43851dc6a9bbfb95a811969
Ajay Chaudhary <43502595+ajaychaudhary7@users.noreply.github.com>
2020-06-11 23:39:51 +0530

    Add files via upload

---
+++ Area Calculation/object_size.py
@@ -0,0 +1,147 @@
+# USAGE
+#python object_size.py --image images/3.jpg --width 1
+
+from scipy.spatial import distance as dist
+from imutils import perspective
+from imutils import contours
+import numpy as np
+import argparse
+import imutils
+import cv2
+
+def midpoint(ptA, ptB):
+	return ((ptA[0] + ptB[0]) * 0.5, (ptA[1] + ptB[1]) * 0.5)
+
+# construct the argument parse and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", required=True,
+	help="path to the input image")
+ap.add_argument("-w", "--width", type=float, required=True,
+	help="width of the left-most object in the image (in inches)")
+args = vars(ap.parse_args())
+
+image = cv2.imread(args["image"])
+
+
+def view():
+	boundaries = [([100, 0, 0], [255, 200, 190])]
+	
+	img = []
+    
+	for (lower, upper) in boundaries:
+		# create NumPy arrays from the boundaries
+		lower = np.array(lower, dtype = "uint8")
+		upper = np.array(upper, dtype = "uint8")
+ 
+		# find the colors within the specified boundaries and apply
+		# the mask
+		mask = cv2.inRange(image, lower, upper)
+		output = cv2.bitwise_and(image, image, mask = mask)
+		area(output)
+		img.append(output)
+
+		#cv2.imshow("images", np.hstack([image, output]))
+		#cv2.waitKey(0)
+	area(img)
+
+
+def area(img):
+	#cv2.imshow("Input",img)
+	#cv2.waitKey(0)
+	img1 = np.uint8(img)
+	edged = cv2.Canny(img1, 50, 100)
+	edged = cv2.dilate(edged, None, iterations=1)
+	edged = cv2.erode(edged, None, iterations=1)
+
+	# find contours in the edge map
+	cnts = cv2.findContours(edged.copy(), cv2.RETR_EXTERNAL,
+		cv2.CHAIN_APPROX_SIMPLE)
+	cnts = imutils.grab_contours(cnts)
+
+	if len(cnts) > 0:
+		cnts = contours.sort_contours(cnts)[0]
+	#(cnts, _) = contours.sort_contours(cnts)
+	pixelsPerMetric = None
+
+	total_area = 0
+	base = 0
+
+	for c in cnts:
+		# if the contour is not sufficiently large, ignore it
+		if cv2.contourArea(c) < 100:
+			continue
+
+		#print()
+
+		# compute the rotated bounding box of the contour
+		orig = image.copy()
+		box = cv2.minAreaRect(c)
+		box = cv2.cv.BoxPoints(box) if imutils.is_cv2() else cv2.boxPoints(box)
+		box = np.array(box, dtype="int")
+
+		# order the points in the contour such that they appear
+		# in top-left, top-right, bottom-right, and bottom-left
+		# order, then draw the outline of the rotated bounding
+		# box
+		box = perspective.order_points(box)
+		cv2.drawContours(orig, [box.astype("int")], -1, (0, 255, 0), 2)
+
+		# loop over the original points and draw them
+		for (x, y) in box:
+			cv2.circle(orig, (int(x), int(y)), 5, (0, 0, 255), -1)
+
+		# unpack the ordered bounding box, then compute the midpoint
+		# between the top-left and top-right coordinates, followed by
+		# the midpoint between bottom-left and bottom-right coordinates
+		(tl, tr, br, bl) = box
+		(tltrX, tltrY) = midpoint(tl, tr)
+		(blbrX, blbrY) = midpoint(bl, br)
+
+		# compute the midpoint between the top-left and top-right points,
+		# followed by the midpoint between the top-righ and bottom-right
+		(tlblX, tlblY) = midpoint(tl, bl)
+		(trbrX, trbrY) = midpoint(tr, br)
+
+		# draw the midpoints on the image
+		cv2.circle(orig, (int(tltrX), int(tltrY)), 5, (255, 0, 0), -1)
+		cv2.circle(orig, (int(blbrX), int(blbrY)), 5, (255, 0, 0), -1)
+		cv2.circle(orig, (int(tlblX), int(tlblY)), 5, (255, 0, 0), -1)
+		cv2.circle(orig, (int(trbrX), int(trbrY)), 5, (255, 0, 0), -1)
+
+		# draw lines between the midpoints
+		cv2.line(orig, (int(tltrX), int(tltrY)), (int(blbrX), int(blbrY)),
+			(255, 0, 255), 2)
+		cv2.line(orig, (int(tlblX), int(tlblY)), (int(trbrX), int(trbrY)),
+			(255, 0, 255), 2)
+
+		# compute the Euclidean distance between the midpoints
+		dA = dist.euclidean((tltrX, tltrY), (blbrX, blbrY))
+		dB = dist.euclidean((tlblX, tlblY), (trbrX, trbrY))
+
+		# if the pixels per metric has not been initialized, then
+		# compute it as the ratio of pixels to supplied metric
+		# (in this case, inches)
+		if pixelsPerMetric is None:
+			pixelsPerMetric = dB / args["width"]
+
+		# compute the size of the object
+		dimA = dA / pixelsPerMetric
+		dimB = dB / pixelsPerMetric
+
+		area = cv2.contourArea(c)
+		
+		font = cv2.FONT_HERSHEY_SIMPLEX 
+		org = (00, 185)
+		fontScale = 1 
+		color = (0, 0, 255)
+
+		cv2.putText(orig,"Area is {}".format(area), org, font, fontScale,color, 2, cv2.LINE_AA)
+
+		# show the output image
+		cv2.imshow("Image", orig)
+		cv2.waitKey(0)
+
+view()
+area(image)
+
+
